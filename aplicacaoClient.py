cdee1b0b097b34075de43a1903573d04664f0029
Cesar0106 <cesarea@al.insper.edu.br>
2022-03-10 13:37:04 -0500

    Não funciona ainda

--- aplicacaoClient.py
+++ aplicacaoClient.py
@@ -10,10 +10,13 @@
 #para acompanhar a execução e identificar erros, construa prints ao longo do código! 
 
 
+from ctypes import sizeof
+from http.cookiejar import MozillaCookieJar
 from enlace import *
 import numpy as np
 import time
 import random
+import math
 
 # voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
 #   para saber a sua porta, execute no terminal :
@@ -25,31 +28,61 @@ serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 #serialName = "ACM0"                  # Windows(variacao de)
 
-def comandos():
-    n = random.randint(10,30)
-    print(f"Essa lista tera {n}")
-    c1 = [b'\x00' b'\xFF' b'\x00' b'\xFF']
-    c2 = [b'\x00' b'\xFF' b'\xFF' b'\x00']
-    c3 = [b'\xFF']
-    c4 = [b'\x00']
-    c5 = [b'\xFF' b'\x00']
-    c6 = [b'\x00' b'\xFF']
-    cs = [c1,c2,c3, c4, c5, c6]
+def makeHead(arquivo, tipo_mensagem):
+    """
+    b0 = tipo de mensagem
+    b1 = tamanho do arquivo
+    b2 = Total de pacotes
+    b3 = numero do payload atual
+    b4 = tem ou não pacote com menos de 114
+    b5 = tamanho do payload atual
+    b6 = byte eop
+    """
+    heads = []
+    tamanhoBytes = len(arquivo)
     i = 0
-    cl = []
-    cf = []
-    while i < n:
-        x = random.choice(cs)
-        cf.append(x)
-        if x == c1 or x == c2:
-            cl.append([(4).to_bytes(2, 'big')])
-        elif x == c3 or x == c4:
-            cl.append([(1).to_bytes(2, 'big')])
-        else:
-            cl.append([(2).to_bytes(2, 'big')])
-        cl.append(x)
+    qtdPayloads = math.ceil(tamanhoBytes/114)
+    eop = [b"\xFF",b"\xFF",b"\xFF",b"\xFF"]
+
+
+    while( i <= qtdPayloads):
+        heads.append([tipo_mensagem.to_bytes(1, 'big'), tamanhoBytes.to_bytes(2, 'big'), qtdPayloads.to_bytes(1, 'big') ,i.to_bytes(1, 'big'),(tamanhoBytes - 114*i).to_bytes(1, 'big'),eop])
+        print(heads[i])
         i += 1
-    return cl, cf
+    print("1")
+    return heads
+    
+
+def makePayload(arquivo, heads):
+    tamanhoBytes = len(arquivo)
+    x = 0
+    i = 0
+    payloads = []
+    print(int.from_bytes(heads[x][4], byteorder="big"), len(heads))
+    print(arquivo)
+    while x <= len(heads):
+        payload = []
+        while i <= int.from_bytes(heads[x][4], byteorder="big"):
+            payload.append(arquivo[i])
+            i += 1
+        print(i)
+        payloads.append(payload)   
+        x += 1
+
+    print("2")
+    return payloads
+
+def makeDatagrama(arquivo, tipo):
+    datagramas = []
+    heads = makeHead(arquivo, tipo)
+    payloads = makePayload(arquivo, heads)
+    eop = [b"\xFF",b"\xFF",b"\xFF",b"\xFF"]
+    i = 0
+    while i <= len(heads):
+        datagramas.append(heads[i])
+        datagramas.append(payloads[i])
+        datagramas.append(eop)
+    return datagramas
 
 def main():
     try:
@@ -57,20 +90,45 @@ def main():
         #para declarar esse objeto é o nome da porta.
         com1 = enlace(serialName)
         start_time = time.time()
-
-        
-    
         # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
         if com1.enable() == True:
             print("Comunicação Aberta")
         #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
-        
+        #Criando e enviando Handshake
+        #Tipos:
+        hand = 0
+        arquivoPrincipal = 1
+        arquivoHanshake = [b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00",b"\x00"]
+        print("Enviando Handshake")
+        handshake = (makeDatagrama(arquivoHanshake, hand))
+        print("Handshake gerado: {handshake}")
+        com1.sendData(np.asarray(handshake))
+        print("Esperando Confirmação")
+        inicio = time.time()
+        tempo = False
+        while tempo == False:
+            if (inicio - time.time()) >= 5:
+                tamComando, nRx = com1.getData(10)
+                tempo = True
+            else:
+                resposta = (input("Servidor inativo. Tentar novamente? S/N"))
+                if(resposta.upper() == "S"):
+                    inicio = time.time()
+                else:
+                    print("-------------------------")
+                    print("Comunicação encerrada")
+                    print("-------------------------")
+                    com1.disable()
+                    print("--- {:.4f} seconds ---".format(time.time() - start_time))
+                    tempo = True
+
+        print("Tamanho do Comando", tamComando) 
+        tamanhoRecebido = int.from_bytes(tamComando, byteorder="big")
+        print("tamanhoRecebido: " , tamanhoRecebido/2)
         #aqui você deverá gerar os dados a serem transmitidos. 
         #seus dados a serem transmitidos são uma lista de bytes a serem transmitidos. Gere esta lista com o 
         #nome de txBuffer. Esla sempre irá armazenar os dados a serem enviados.
-        print("Carregando Lista de Comandos para transmissão")
-        txBuffer, comandofinal = comandos()
         print(txBuffer)
         tamanhoLista= (len(txBuffer))
         print("Enviando Tamanho da lista")
